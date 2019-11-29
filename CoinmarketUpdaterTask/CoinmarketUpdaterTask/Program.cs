983c3e9a9742f1c830af112b137192c015e94268
Mariusz Borkowski <mariusz.borkowski@rocketmail.com>
2019-11-29 17:56:06 +0100

    Master

---
+++ CoinmarketUpdaterTask/CoinmarketUpdaterTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoinmarketUpdaterTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
